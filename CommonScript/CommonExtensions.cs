a07dd9d145a66de81bc2a207b96d932813276a9f
Héctor Francés <32682445+hfrances@users.noreply.github.com>
2023-06-27 14:44:38 +0200

    feat: add more extension methods + add comments.

--- CommonScript/CommonExtensions.cs
+++ CommonScript/CommonExtensions.cs
@@ -23,14 +23,27 @@ namespace IngameScript
     static class CommonExtensions
     {
 
-        public static IList<IMyTerminalBlock> GetBlocks(this IMyBlockGroup blockGroup)
+        /// <summary>
+        /// Get terminal blocks which assigned to this group.
+        /// </summary>
+        /// <param name="blockGroup">The block group of the blocks.</param>
+        /// <param name="collect">if function returns true, block would be added to blocks.</param>
+        /// <returns>A list with the terminal blocks assigned to this group.</returns>
+        public static IList<IMyTerminalBlock> GetBlocks(this IMyBlockGroup blockGroup, Func<IMyTerminalBlock, bool> collect = null)
         {
             var blocks = new List<IMyTerminalBlock>();
 
-            blockGroup.GetBlocks(blocks);
+            blockGroup.GetBlocks(blocks, collect);
             return blocks;
         }
 
+        /// <summary>
+        /// Get terminal blocks which assigned to this group, and matching type.
+        /// </summary>
+        /// <typeparam name="T">Block must be assignable from T.</typeparam>
+        /// <param name="blockGroup">The block group of the blocks.</param>
+        /// <param name="collect">if function returns true, block would be added to blocks.</param>
+        /// <returns>A list with the terminal blocks assigned to this group.</returns>
         public static IList<T> GetBlocksOfType<T>(this IMyBlockGroup blockGroup, Func<T, bool> collect = null) where T : class
         {
             var blocks = new List<T>();
@@ -39,6 +52,12 @@ namespace IngameScript
             return blocks;
         }
 
+        /// <summary>
+        /// Returns a list with the blocks reachable by this grid terminal system.
+        /// This means all blocks on the same grid, or connected via rotors, pistons or connectors.
+        /// </summary>
+        /// <param name="gridTerminalSystem">The terminal server of the blocks.</param>
+        /// <returns>A list with the blocks.</returns>
         public static IList<IMyTerminalBlock> GetBlocks(this IMyGridTerminalSystem gridTerminalSystem)
         {
             var blocks = new List<IMyTerminalBlock>();
@@ -47,6 +66,14 @@ namespace IngameScript
             return blocks;
         }
 
+        /// <summary>
+        /// Returns a list with the blocks reachable by this grid terminal system.
+        /// This means all blocks on the same grid, or connected via rotors, pistons or connectors.
+        /// </summary>
+        /// <typeparam name="T">The type of blocks to retrieve.</typeparam>
+        /// <param name="gridTerminalSystem">The terminal server of the blocks.</param>
+        /// <param name="collect">Provide a filter method to determine if a given group should be added or not.</param>
+        /// <returns>A list with the blocks.</returns>
         public static IList<T> GetBlocksOfType<T>(this IMyGridTerminalSystem gridTerminalSystem, Func<T, bool> collect = null) where T : class
         {
             var blocks = new List<T>();
@@ -55,6 +82,12 @@ namespace IngameScript
             return blocks;
         }
 
+        /// <summary>
+        /// Gets the block groups reachable by this grid terminal system.
+        /// </summary>
+        /// <param name="gridTerminalSystem">The terminal server of the block groups.</param>
+        /// <param name="collect">Provide a filter method to determine if a given group should be added or not.</param>
+        /// <returns>A list with the block groups.</returns>
         public static IList<IMyBlockGroup> GetBlockGroups(this IMyGridTerminalSystem gridTerminalSystem, Func<IMyBlockGroup, bool> collect = null)
         {
             var blockGroups = new List<IMyBlockGroup>();
@@ -64,7 +97,30 @@ namespace IngameScript
         }
 
         /// <summary>
-        /// Get all terminal actions available for block.
+        /// Gets the block groups where the <see cref="IMyTerminalBlock"/> is in.
+        /// </summary>
+        /// <param name="gridTerminalSystem">The terminal server of the block groups.</param>
+        /// <param name="block">The terminal block to find in the block groups.</param>
+        /// <param name="collect">Provide a filter method to determine if a given group should be added or not.</param>
+        /// <returns>A list with the block groups.</returns>
+        public static IList<IMyBlockGroup> GetBlockGroups(this IMyGridTerminalSystem gridTerminalSystem, IMyTerminalBlock block, Func<IMyBlockGroup, bool> collect = null)
+        {
+            Func<IMyBlockGroup, bool> basicPredicate = x => x.GetBlocks().Any(y => y == block);
+            Func<IMyBlockGroup, bool> predicate;
+
+            if (collect == null)
+            {
+                predicate = basicPredicate;
+            }
+            else
+            {
+                predicate = x => basicPredicate(x) && collect(x);
+            }
+            return GetBlockGroups(gridTerminalSystem, predicate);
+        }
+
+        /// <summary>
+        /// Gets all terminal actions available for block.
         /// </summary>
         public static IList<ITerminalAction> GetActions(this IMyTerminalBlock block, Func<ITerminalAction, bool> collect = null)
         {
